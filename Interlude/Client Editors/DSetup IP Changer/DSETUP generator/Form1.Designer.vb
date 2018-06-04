993a3a4c698adc2dd86102e31f78dae40ef007e0
NightWolf BlackCode <NightW0lv@hotmail.com>
2018-06-04 12:33:23 +0300

    Addition of DSetup IP Changer

---
+++ Interlude/Client Editors/DSetup IP Changer/DSETUP generator/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(27, 32)
+        Me.TextBox1.MaxLength = 15
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(174, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(7, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(20, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "IP:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(107, 58)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(96, 27)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "GENERATE"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(2, 3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(199, 26)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Fill in the server" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "IP here add the new dll in \system folder."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(215, 91)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(327, 173)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(180, 130)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "DSETUP generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ Interlude/Client Editors/DSetup IP Changer/DSETUP generator/Form1.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Text.Encoding
+Imports System.IO
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+    Public Function Is_numeric(ByVal key As Keys)
+        Select Case key
+            Case Keys.D0, Keys.D1, Keys.D2, Keys.D3, Keys.D4, Keys.D5, Keys.D6, Keys.D7, Keys.D8, Keys.D9, 46, Keys.Back
+                Return True
+        End Select
+        Return False
+    End Function
+
+    Private Sub TextBox1_KeyPress(ByVal sender As Object, ByVal e As KeyPressEventArgs) Handles TextBox1.KeyPress
+        If Not Is_numeric(Asc(e.KeyChar)) Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As EventArgs) Handles Button1.Click
+        Try
+            If Regex.IsMatch(TextBox1.Text, "\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b") Then
+                File.WriteAllBytes(Application.StartupPath & "\DSETUP.dll", My.Resources.DSETUP)
+                Dim bw As BinaryWriter = New BinaryWriter(File.Open(Application.StartupPath & "\DSETUP.dll", FileMode.Open, FileAccess.ReadWrite))
+                bw.BaseStream.Seek(&H545C, SeekOrigin.Begin)
+                bw.Write([Default].GetBytes(TextBox1.Text))
+                For i = 0 To (15 - TextBox1.Text.Length) - 1
+                    bw.Write(False)
+                Next
+                bw.Close()
+                MsgBox("Patch successful", MsgBoxStyle.Information, "DSETUP")
+            Else
+                MsgBox("This is not a valid IP!", 16, "Error")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
